5bba42f012f98a648fe01f680c29512506f842e9
Dora Loginova <doris-i@mail.ru>
2024-03-14 16:49:58 +0300

    Add workflow with PEP8 linting

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.2.143', '127.0.0.1', 'localhost', 
-'doralogtask.ddns.net']
+ALLOWED_HOSTS = ['158.160.2.143', '127.0.0.1', 'localhost',
+                 'doralogtask.ddns.net']
 
 
 # Application definition
